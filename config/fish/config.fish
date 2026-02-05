a4571cebdfc60bf9d712e80090a77cffe2a11b0b
arunkumartm68 <arunkumartm68@gmail.com>
2026-02-05 21:10:42 +0530

    Initial Hyprland dotfiles (Nord theme)

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	neofetch
+end
+
+set -x XCURSOR_PATH $XCURSOR_PATH ~/.local/share/icons/:~/.icons/:/usr/share/icons/
+# set -x XCURSOR_SIZE 64
+# set -x XCURSOR_THEME Furina-v2
